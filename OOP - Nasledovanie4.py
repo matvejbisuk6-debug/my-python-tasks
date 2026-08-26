a9d8472a0b6bd0707a38e62cf02d25ab0747308c
Matvey <matvejbisuk6@gmail.com>
2026-08-26 23:52:31 +0300

    Add files via upload

--- OOP - Nasledovanie4.py
+++ OOP - Nasledovanie4.py
@@ -52,3 +52,45 @@ print(character.take_damage(5, 2, "Тролль1 убит"))
 print(boss.take_damage(15, 5, "Тролль2 приспешник убит"))
 print(boss.take_damage_boss(25, 25, "Король Троллей убит"))
 
+#Задача 2
+class User:
+    def __init__(self, name: str, year_old: int):
+        self.name = name
+        self.year_old = year_old
+
+class Base_Command(User):
+    def __init__(self, name: str, year_old: int, command_start: bool):
+        super().__init__(name, year_old)
+        self.command_start = command_start
+
+    def execute(self):
+        if self.command_start == True and self.year_old >= 18:
+            return f"Поздравляем! {self.name} Телеграмм бот запущен!"
+        else:
+            return "Error"
+
+class SettingsCommand(User):
+    def __init__(self, name: str, year_old: int, command_settings: str):
+        super().__init__(name, year_old)
+        self.command_settings = command_settings
+
+    def execute(self):
+        return f"Команда /settings включена, язык - {self.command_settings}"
+
+class HelpCommand(User):
+    def __init__(self, name: str, year_old: int, help_command: bool):
+        super().__init__(name, year_old)
+        self.help_command = help_command
+
+    def execute(self, text: str):
+        if self.help_command == True and self.year_old >= 18:
+            return f"Поздравляем, {self.name}! Завтра мы поможем вам {text}"
+        else:
+            return "Мы не поможем вам."
+
+base = Base_Command("Matvey", 19, True)
+setting = SettingsCommand("Matvey", 19, "Немецкий")
+help = HelpCommand("Matvey", 19, True)
+print(base.execute())
+print(setting.execute())
+print(help.execute("помириться с другом"))
