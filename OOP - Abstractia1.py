6adac719e1900924010f2aac66afef61efff1a96
Matvey <matvejbisuk6@gmail.com>
2026-09-14 00:26:06 +0300

    Add files via upload

--- OOP - Abstractia1.py
+++ OOP - Abstractia1.py
@@ -22,3 +22,33 @@ my_bike = Motorcycle()
 
 print(my_car.start_engine())
 print(my_bike.start_engine())
+
+#Задача 2
+class Instrument(ABC):
+
+    @abstractmethod
+    def play_sound(self) -> None:
+        pass
+
+class Guitar(Instrument):
+
+    def play_sound(self) -> str:
+        return "Брынь-брынь!"
+
+class Drum(Instrument):
+
+    def play_sound(self) -> str:
+        return "Бум-бам!"
+
+class Piano(Instrument):
+
+    def play_sound(self) -> str:
+        return "Ля-ля-ля"
+
+guitar = Guitar()
+drum = Drum()
+piano = Piano()
+
+print(guitar.play_sound())
+print(drum.play_sound())
+print(piano.play_sound())
