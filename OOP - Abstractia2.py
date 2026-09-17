4d03092c096774dd3e9bfd9fc4dfbe4af76ec303
Matvey <matvejbisuk6@gmail.com>
2026-09-18 00:13:51 +0300

    Add files via upload

--- OOP - Abstractia2.py
+++ OOP - Abstractia2.py
@@ -22,3 +22,32 @@ refrigerator = Refrigerator()
 
 print(washing_machine.turn_on())
 print(refrigerator.turn_on())
+
+#Задача 2
+class AuthService(ABC):
+
+    @abstractmethod
+    def login(self, username: str, password: int) -> None:
+        pass
+
+class PasswordAuth(AuthService):
+
+    def login(self, username: str, password: str) -> str:
+        if password == "1234":
+            return True
+        else:
+            return False
+
+class SmsCodeAuth(AuthService):
+
+    def login(self, username: str, password: str) -> str:
+        if len(password) == 4:
+            return True
+        else:
+            return False
+
+password_auth = PasswordAuth()
+sms_auth = SmsCodeAuth()
+
+print(password_auth.login("Matvey", "1234"))
+print(sms_auth.login("Matvey", "1234"))
